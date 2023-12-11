68edbb5fc6fda223b23e814ae966bab2257be743
leonnSanchez <130141610+leonnSanchez@users.noreply.github.com>
2023-12-11 14:00:11 -0300

    Update pantallas.js

--- pantallas.js
+++ pantallas.js
@@ -28,7 +28,7 @@ class Pantalla {
     this.textos[0][3] = "ya despierto decide salir a caminar y buscar algo que lo entretenga";
     this.textos[1][3] = "ya que ultimamente sus dias son aburridos";
     this.textos[0][4] = "se toma un vaso de leche y se queda pensando en lo triste que es su vida quedanso siempre ahi";
-    this.textos[1][4] = "";
+    this.textos[1][4] = "vaso tras vaso toma sin parar";
     this.textos[0][5] = "al tomar todo el vaso de leche se queda completamente dormido y pacifico";
     this.textos[1][5] = "al final del dia es lo que mas lo hace feliz";
     this.textos[0][6] = "en el camino se encuentra una señora que le dice que unos lobos estan molestando en el bosque";
